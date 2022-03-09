d69797b50addd94a9d241ec923ab5bb050ed6120
P-000129\MyoMinHtay <scm.myominhtay@gmail.com>
2022-03-09 13:22:27 +0630

    [feature/Tutorial1] Completed 100%

---
+++ Tutorial1/Module1.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim i, j, r As Integer
+        r = 6
+
+        For i = 0 To r
+
+            For j = 1 To r - i
+                Console.Write(" ")
+            Next
+
+            For j = 1 To 2 * i - 1
+                Console.Write("*")
+            Next
+
+            Console.Write(vbLf)
+        Next
+
+        For i = r - 1 To 0 Step -1
+
+            For j = 1 To r - i
+                Console.Write(" ")
+            Next
+
+            For j = 1 To 2 * i - 1
+                Console.Write("*")
+            Next
+
+            Console.Write(vbLf)
+        Next
+
+        Console.ReadLine()
+    End Sub
+
+End Module
