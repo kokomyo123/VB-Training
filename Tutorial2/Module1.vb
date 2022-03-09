b58462e671e3421b47e675a675591774102da4bb
P-000129\MyoMinHtay <scm.myominhtay@gmail.com>
2022-03-09 13:29:13 +0630

    [feature/Tutorial2] Completed 100%

---
+++ Tutorial2/Module1.vb
@@ -0,0 +1,23 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim year As Integer
+        Console.Write("Input year : ")
+        year = Convert.ToInt32(Console.ReadLine())
+        GetCenturyAndCheckLeapYear(year)
+        Console.ReadLine()
+    End Sub
+
+    Public Sub GetCenturyAndCheckLeapYear(ByVal year As Integer)
+        Dim century As Integer
+
+        If year Mod 400 = 0 OrElse year >= 1000 AndAlso year Mod 4 = 0 AndAlso year Mod 100 <> 0 Then
+            century = (year / 100) + 1
+            Console.WriteLine(century & "," & 1)
+        Else
+            century = (year / 100) + 1
+            Console.WriteLine(century & "," & (-1))
+        End If
+    End Sub
+
+End Module
---
+++ Tutorial2/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Tutorial2")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Tutorial2")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("a3bd4665-e177-41a4-93f8-2d52e7b8946a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
