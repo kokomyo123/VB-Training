0d88b8b9c4b2d91cb76f0bde374e3f7dc4cf420a
P-000129\MyoMinHtay <scm.myominhtay@gmail.com>
2022-03-09 17:05:28 +0630

    [feature/Tutorial9] Completed 100%

---
+++ Tutorial9/Main.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Main.aspx.vb" Inherits="Tutorial9.Main" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="btnExcel" runat="server" Text="Fetching data from Excel" OnClick="btnExcel_Click" BackColor="#99FFCC" BorderStyle="Groove" Font-Bold="True" Font-Size="Large" Height="50px" Width="300px" Style="cursor: pointer" /><br />
+            <br />
+            <br />
+            <asp:Button ID="btnCSV" runat="server" OnClick="btnCSV_Click" Text="Fetching data from CSV file" BackColor="#99FFCC" BorderStyle="Groove" Font-Bold="True" Font-Size="Large" Height="50px" Width="300px" Style="cursor: pointer" /><br />
+            <br />
+            <br />
+            <asp:Button ID="btnText" runat="server" OnClick="btnText_Click" Text="Fetching data from Text file" BackColor="#99FFCC" BorderStyle="Groove" Font-Bold="True" Font-Size="Large" Height="50px" Width="300px" Style="cursor: pointer" /><br />
+            <br />
+            <br />
+            <asp:Button ID="btnDoc" runat="server" OnClick="btnDoc_Click" Text="Fetching data from Document" BackColor="#99FFCC" BorderStyle="Groove" Font-Bold="True" Font-Size="Large" Height="50px" Width="300px" Style="cursor: pointer" />
+            <br />
+            <br />
+            <asp:GridView ID="gvFile" runat="server" CellPadding="3" GridLines="Horizontal" AutoGenerateColumns="False" BackColor="White" BorderColor="#E7E7FF" BorderStyle="None" BorderWidth="1px">
+                <AlternatingRowStyle BackColor="#F7F7F7" />
+                <FooterStyle BackColor="#B5C7DE" ForeColor="#4A3C8C" />
+                <HeaderStyle BackColor="#4A3C8C" Font-Bold="True" ForeColor="#F7F7F7" />
+                <PagerStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" HorizontalAlign="Right" />
+                <RowStyle BackColor="#E7E7FF" ForeColor="#4A3C8C" />
+                <SelectedRowStyle BackColor="#738A9C" Font-Bold="True" ForeColor="#F7F7F7" />
+                <SortedAscendingCellStyle BackColor="#F4F4FD" />
+                <SortedAscendingHeaderStyle BackColor="#5A4C9D" />
+                <SortedDescendingCellStyle BackColor="#D8D8F0" />
+                <SortedDescendingHeaderStyle BackColor="#3E3277" />
+
+                <Columns>
+                    <asp:BoundField DataField="Id" HeaderText="ID" />
+                    <asp:BoundField DataField="Name" HeaderText="Name" />
+                    <asp:BoundField DataField="Address" HeaderText="Address" />
+                    <asp:BoundField DataField="Email" HeaderText="Email" />
+                </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Tutorial9/Main.aspx.vb
@@ -0,0 +1,137 @@
+﻿Imports System.Data.OleDb
+Imports System.IO
+
+Public Class Main
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btnExcel_Click(sender As Object, e As EventArgs) Handles btnExcel.Click
+        Dim excelfilepath As String = Server.MapPath("~/Upload/User.xlsx")
+
+        If File.Exists(excelfilepath) Then
+            ImoprtExceltoGridView(excelfilepath, ".xlsx", "Yes")
+        Else
+            Response.Write("There is no file to read")
+        End If
+    End Sub
+
+    Public Sub ImoprtExceltoGridView(ByVal File As String, ByVal extension As String, ByVal ishdr As String)
+        Dim connectionStr As String = ConfigurationManager.ConnectionStrings("Excelconnection").ConnectionString
+        connectionStr = String.Format(connectionStr, File, ishdr)
+        Dim con As OleDbConnection = New OleDbConnection(connectionStr)
+        Dim cmd As OleDbCommand = New OleDbCommand()
+        Dim oda As OleDbDataAdapter = New OleDbDataAdapter()
+        Dim dt As DataTable = New DataTable()
+        cmd.Connection = con
+        con.Open()
+        Dim dtexcel As DataTable = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, Nothing)
+        Dim sheetname As String = dtexcel.Rows(0)("TABLE_NAME").ToString()
+        cmd.CommandText = "Select * from [" & sheetname & "]"
+        oda.SelectCommand = cmd
+        oda.Fill(dt)
+        con.Close()
+        gvFile.DataSource = dt
+        gvFile.DataBind()
+    End Sub
+
+    Protected Sub btnCSV_Click(sender As Object, e As EventArgs) Handles btnCSV.Click
+        Dim csvPath As String = Server.MapPath("~/Upload/User.csv")
+
+        If File.Exists(csvPath) Then
+            Dim dt As DataTable = New DataTable()
+            dt.Columns.AddRange(New DataColumn(3) {New DataColumn("Id"), New DataColumn("Name"), New DataColumn("Address"), New DataColumn("Email")})
+            Dim csvData As String = File.ReadAllText(csvPath)
+
+            For Each row As String In csvData.Split(vbLf)
+
+                If Not String.IsNullOrEmpty(row) Then
+                    dt.Rows.Add()
+                    Dim i As Integer = 0
+
+                    For Each cell As String In row.Split(","c)
+                        dt.Rows(dt.Rows.Count - 1)(i) = cell
+                        i += 1
+                    Next
+                End If
+            Next
+
+            gvFile.DataSource = dt
+            gvFile.DataBind()
+        Else
+            Response.Write("There is no file to read")
+        End If
+    End Sub
+
+    Protected Sub btnText_Click(sender As Object, e As EventArgs) Handles btnText.Click
+        Dim csvPath As String = Server.MapPath("~/Upload/User.txt")
+
+        If File.Exists(csvPath) Then
+            Dim dt As DataTable = New DataTable()
+            dt.Columns.AddRange(New DataColumn(3) {New DataColumn("Id"), New DataColumn("Name"), New DataColumn("Address"), New DataColumn("Email")})
+            Dim csvData As String = File.ReadAllText(csvPath)
+
+            For Each row As String In csvData.Split(vbLf)
+
+                If Not String.IsNullOrEmpty(row) Then
+                    dt.Rows.Add()
+                    Dim i As Integer = 0
+
+                    For Each cell As String In row.Split(","c)
+                        dt.Rows(dt.Rows.Count - 1)(i) = cell
+                        i += 1
+                    Next
+                End If
+            Next
+
+            gvFile.DataSource = dt
+            gvFile.DataBind()
+        Else
+            Response.Write("There is no file to read")
+        End If
+    End Sub
+
+    Protected Sub btnDoc_Click(sender As Object, e As EventArgs) Handles btnDoc.Click
+        Dim MyWord As Microsoft.Office.Interop.Word.Application = New Microsoft.Office.Interop.Word.Application()
+        Dim sFile As String = Server.MapPath("~/Upload/User.docx")
+
+        If File.Exists(sFile) Then
+            Dim rstData As DataTable = New DataTable()
+            MyWord.Documents.Open(sFile)
+            Dim MyDoc As Microsoft.Office.Interop.Word.Document = MyWord.ActiveDocument
+            Dim MyTable As Microsoft.Office.Interop.Word.Table = MyDoc.Tables(1)
+            Dim iCol As Integer = 0
+
+            For iCol = 1 To MyTable.Columns.Count
+                rstData.Columns.Add(CleanCellText(MyTable.Rows(1).Cells(iCol).Range.Text), GetType(String))
+            Next
+
+            Dim iRows As Integer = 0
+
+            For iRows = 2 To MyTable.Rows.Count
+                Dim OneRow As DataRow = rstData.NewRow()
+
+                For iCol = 1 To MyTable.Columns.Count
+                    OneRow(iCol - 1) = CleanCellText(MyTable.Rows(iRows).Cells(iCol).Range.Text)
+                Next
+
+                rstData.Rows.Add(OneRow)
+            Next
+
+            MyDoc.Close()
+            MyWord.Quit()
+            gvFile.DataSource = rstData
+            gvFile.DataBind()
+        Else
+            Response.Write("There is no file to read")
+        End If
+    End Sub
+
+    Private Function CleanCellText(ByVal s As String) As String
+        Dim MyClean As String = (ChrW(13)).ToString() & (ChrW(7)).ToString()
+        Return s.Replace(MyClean, "")
+    End Function
+
+End Class
